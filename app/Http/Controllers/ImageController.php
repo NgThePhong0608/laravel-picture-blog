fda8a6247e1646c414c8f7e4e062ec6eae3acb01
PhongNT <phongnt@kaopiz.com>
2024-02-17 01:28:02 +0700

    add pagination view

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -10,7 +10,7 @@ class ImageController extends Controller
 {
     public function index(Request $request)
     {
-        $images = Image::published()->latest()->paginate(15);
+        $images = Image::published()->latest()->paginate(3)->withQueryString();
         return view('image.index', compact('images'));
     }
 
