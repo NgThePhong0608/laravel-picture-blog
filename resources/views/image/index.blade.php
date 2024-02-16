80874acde7ceded0d7359401b2326b0fa27521ec
PhongNT <phongnt@kaopiz.com>
2024-02-17 00:43:59 +0700

    create layout and integrating to application

--- resources/views/image/index.blade.php
+++ resources/views/image/index.blade.php
@@ -1,21 +1,24 @@
-<h1>All Images</h1>
+<x-layout title="Discover free images">
+    <h1>All Images</h1>
 
-<a href="{{ route('images.create') }}">Upload image</a>
+    <a href="{{ route('images.create') }}">Upload image</a>
 
-@if($message = session('message'))
-    <div>{{ $message }}</div>
-@endif
+    @if($message = session('message'))
+        <div>{{ $message }}</div>
+    @endif
 
-@foreach($images as $image)
-    <div>
-        <a href="{{ $image->permalink() }}">
-            <img src="{{ $image->fileUrl() }}" alt="{{ $image->title }}" width="300">
-        </a>
+    @foreach($images as $image)
         <div>
-            <a href="{{ $image->route('edit') }}">Edit</a>
-            <x-form action="{{ $image->route('destroy') }}" method="DELETE" style="display: inline">
-                <button type="submit" onclick="return confirm('Are you sure ?')">Delete</button>
-            </x-form>
+            <a href="{{ $image->permalink() }}">
+                <img src="{{ $image->fileUrl() }}" alt="{{ $image->title }}" width="300">
+            </a>
+            <div>
+                <a href="{{ $image->route('edit') }}">Edit</a>
+                <x-form action="{{ $image->route('destroy') }}" method="DELETE" style="display: inline">
+                    <button type="submit" onclick="return confirm('Are you sure ?')">Delete</button>
+                </x-form>
+            </div>
         </div>
-    </div>
-@endforeach
+    @endforeach
+
+</x-layout>
