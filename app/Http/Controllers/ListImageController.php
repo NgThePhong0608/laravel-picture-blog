d309448369e0375102e240378d6cbd552ee2e326
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-02-17 03:24:05 +0700

    create new page to display public image

---
+++ app/Http/Controllers/ListImageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Image;
+use Illuminate\Http\Request;
+
+class ListImageController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $images = Image::published()->latest()->paginate(3)->withQueryString();
+        return view('image-list', compact('images'));
+    }
+}
