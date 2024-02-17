143220b40bde067be8b96455ddb5f62e9b2ae2b1
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-02-17 22:23:58 +0700

    social seeder data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Image;
+use App\Models\Social;
+use App\Models\User;
 use Database\Factories\ImageFactory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Storage;
@@ -25,5 +27,9 @@ class DatabaseSeeder extends Seeder
                 'dimension' => Image::getDimension($image),
             ]);
         }
+
+        User::find([2, 4, 6])->each(function ($user){
+            $user->social()->save(Social::factory()->make());
+        });
     }
 }
