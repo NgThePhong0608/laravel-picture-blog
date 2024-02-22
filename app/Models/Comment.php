7e79e7aafc67e56d985d8d02b5c936e8a220fac3
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-02-22 21:29:29 +0700

    Mederating comments

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,15 +9,14 @@ class Comment extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['body', 'user_id', 'image_id'];
+    protected $fillable = ['body', 'user_id', 'image_id', 'approved'];
 
     public function image()
     {
-        return $this->belongsTo(Image::class);
-    }
-
-    public function user()
-    {
         return $this->belongsTo(User::class);
+    }
+    public function scopeApproved(Builder $query)
+    {
+        return $query->where('approved', true);
     }
 }
