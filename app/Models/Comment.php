33e990a8c2a1bbadfdcfd44edf0244f186d8ee81
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-02-22 21:53:17 +0700

    refactor code

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,6 +13,11 @@ class Comment extends Model
     protected $fillable = ['body', 'user_id', 'image_id', 'approved'];
 
     public function image()
+    {
+        return $this->belongsTo(Image::class);
+    }
+
+    public function user()
     {
         return $this->belongsTo(User::class);
     }
--- app/Models/Image.php
+++ app/Models/Image.php
@@ -6,7 +6,6 @@ use App\Enum\Role;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Session\Store;
 use Illuminate\Support\Facades\Storage;
 
 class Image extends Model
